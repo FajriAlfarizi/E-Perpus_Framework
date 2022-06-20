1433f53afb683fbeca03e7490c2d35472d84394a
FajriAlfarizi <92989089+FajriAlfarizi@users.noreply.github.com>
2022-06-20 21:31:40 +0800

    UASFM

---
+++ dpw/app/admin/pages/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+        <b>Versi</b> 1.0
+    </div>
+    <strong>Kelompok 3 &copy; <?= date('Y'); ?>. Project</strong> Rekayasa Perangkat Lunak
+</footer>
---
+++ dpw/app/user/pages/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+        <b>Versi</b> 1.0
+    </div>
+    <strong>Kelompok 4 &copy; <?= date('Y'); ?>. Project</strong> Desain Pemrograman Web.
+</footer>
